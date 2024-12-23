84470022f612a4a919047f122d639315bbed968f
Kumail Haider <kumail074@proton.me>
2024-12-23 23:46:46 +0500

    code

--- src/main.c
+++ src/main.c
@@ -8,7 +8,7 @@
 //#include <errno.h>
 #include <stdbool.h>
 //#include <stddef.h>
-//#include <stdint.h>
+#include <stdint.h>
 //#include <pthread.h>
 
 #define COLUMN_USERNAME_SIZE 32
@@ -52,7 +52,10 @@ typedef struct {
 const uint32_t ID_SIZE = size_of_attribute(Row, id);
 const uint32_t USERNAME_SIZE = size_of_attribute(Row, username);
 const uint32_t EMAIL_SIZE = size_of_attribute(Row, email);
-
+const uint32_t ID_OFFSET = 0;
+const uint32_t USERNAME_OFFSET = ID_OFFSET + ID_SIZE;
+const uint32_t EMAIL_OFFSET = USERNAME_OFFSET + USERNAME_SIZE;
+const uint32_t ROW_SIZE = ID_SIZE + USERNAME_SIZE + EMAIL_SIZE;
 
 InputBuffer* new_input_buffer() {
     InputBuffer* input_buffer = (InputBuffer*)malloc(sizeof(InputBuffer));
